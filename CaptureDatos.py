2f8bb561e5b860215b8c251248ea00842d6b9e8c
clairedaba <clairedabadiop98@gmail.com>
2023-11-01 16:47:34 -0400

    modoficacion de MessageQueue usando hilos para permitir varios clientes al mismo tiempo

--- CaptureDatos.py
+++ CaptureDatos.py
@@ -9,3 +9,20 @@ PORT = 2500
 
 #Generar formularios en formatos json
 
+def generate_form():
+    form_data = {
+        'field1': random.randint(1, 100),
+        'field2': random.choice(['A', 'B', 'C']),
+        'timestamp': time.time()
+    }
+    return json.dumps(form_data)
+
+#Crear un socket pqrq connectarse al servidor de mensages
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+
+# Generar y enviar un formulario a la cola de mensajes
+    form = generate_form()
+    client_socket.sendall(form.encode())
+
+    print(f"Formulario enviado a la cola de mensajes: {form}")
--- MessageQueue.py
+++ MessageQueue.py
@@ -6,22 +6,26 @@ import threading
 HOST = 'localhost'
 PORT = 2500
 
-# Crear un socket para escuchar conexiones entrantes
+# Función para manejar la comunicación con un cliente individual
+def handle_client(client_socket):
+    print(f"Conectado por {client_socket.getpeername()}")
+    
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print(f"Mensaje recibido: {data.decode()}")
+    
+    client_socket.close()
 
+# Crear un socket para escuchar conexiones entrantes
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))
     server_socket.listen()
 
     print(f"Escuchando en {HOST}:{PORT}...")
 
-# aceptar una connection entrqnte
-conn, addr = server_socket.accept()
-with conn:
-        print(f"Conectado por {addr}")
-
-        # Escuchar mensajes entrantes y reenviarlos a todos los clientes conectados
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"Mensaje recibido: {data.decode()}")
+    while True:
+        conn, addr = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(conn,))
+        client_thread.start()
